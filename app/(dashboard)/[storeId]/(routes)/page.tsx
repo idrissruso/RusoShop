49aa78415d25f482fe8e95a813218bd480a16a71
Idrissa <idrisabkar2018@gmail.com>
2024-02-25 17:54:22 +0300

    Implemented server-side rendering for DashBoard page with store data fetched from Prisma

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,17 @@
-export default function DashBoard() {
-  return <div>This is a DashBoard</div>
+import prismadb from '@/lib/prismadb'
+
+export default async function DashBoard({
+  params,
+}: {
+  params: {
+    storeId: string
+  }
+}) {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  })
+
+  return <div>The Active Store is {`${store?.name}`}</div>
 }
